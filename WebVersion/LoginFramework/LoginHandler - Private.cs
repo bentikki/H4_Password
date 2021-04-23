9d067faaf1e8271d0f9169b5c68e7a9b0dc895a1
benjatikki <benjamin.tikki@gmail.com>
2021-04-23 19:29:47 +0200

    With_ClassLibrary

---
+++ WebVersion/LoginFramework/LoginHandler - Private.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using LoginFramework.Models;
+using LoginFramework.Helpers;
+using LoginFramework.Hashing;
+using LoginFramework.HashingMethods;
+
+namespace LoginFramework
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class LoginHandler
+    {
+        /// <summary>
+        /// Checks if the Hashing method is supported.
+        /// Throw exception if an unsupported method is chosen.
+        /// Returns IHashingMethod if success.
+        /// </summary>
+        /// <param name="hashingMethod">HashingMethodType to check - returns the corresponding IHashingMethod.</param>
+        /// <returns></returns>
+        private IHashingMethod ViableHashingMethodCheck(HashingMethodType hashingMethod)
+        {
+            IHashingMethod method = null;
+
+            switch (hashingMethod)
+            {
+                case HashingMethodType.SHA256:
+                    method = new HashingSHA256();
+                    break;
+                default: throw new ArgumentException("Not a viable hashing method.", "hashingMethod");
+            }
+
+            return method;
+        }
+
+    }
+}
--- WebVersion/LoginFramework/Models/IHashedUser.cs
+++ WebVersion/LoginFramework/Models/IHashedUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace LoginFramework.Models
@@ -7,7 +8,10 @@ namespace LoginFramework.Models
     public interface IHashedUser
     {
         string Username { get; set; }
+       
+        [Required]
         string Password { get; set; }
+        
         string Salt { get; set; }
         byte[] SaltByteArray { get; set; }
     }
